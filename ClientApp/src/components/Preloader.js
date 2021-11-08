be710e81b16ecd503a01405f2d91afaa325a6fe5
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-11-08 05:33:34 +0200

    Added preloader component

---
+++ ClientApp/src/components/Preloader.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+function Preloader() {
+
+    return (
+        <>
+            <div id="preloader"></div>
+            <a href="index.html" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
+        </>
+    );
+}
+
+export { Preloader };
