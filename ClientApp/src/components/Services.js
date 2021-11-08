73fbdb2e1e2e0a63dcc9484a35f4242bbeb26009
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-11-08 05:32:32 +0200

    Added services components

---
+++ ClientApp/src/components/Services.js
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import { Service } from './Service';
+
+function Services() {
+
+    return (
+        <section className="section-services section-t8">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="title-wrap d-flex justify-content-between">
+                            <div className="title-box">
+                                <h2 className="title-a">Our Services</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+
+                    <Service />
+                    <Service />
+                    <Service />
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export { Services };
