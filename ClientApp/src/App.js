35a5d088a122f5729982c122187638d6dbf7d039
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-11-08 03:34:12 +0200

    Changed layout

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -8,7 +8,6 @@ import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
 
-import './custom.css'
 
 export default class App extends Component {
   static displayName = App.name;
