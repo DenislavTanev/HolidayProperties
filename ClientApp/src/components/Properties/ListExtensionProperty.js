e7fa39b671759b193a2c57dad54748daaeb6dd6c
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-11-14 12:36:12 +0200

    Split property components

---
+++ ClientApp/src/components/Properties/ListExtensionProperty.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import SingleProperty from './SingleProperty';
+
+const ListExtensionProperty = () => {
+
+    return (
+        <div className="col-md-4">
+
+            <SingleProperty />
+
+        </div>
+    );
+};
+
+export default ListExtensionProperty;
---
+++ ClientApp/src/components/Properties/SwiperExtensionProperty.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import SingleProperty from './SingleProperty';
+
+const SwiperExtensionProperty = () => {
+
+    return (
+        <div className="carousel-item-b swiper-slide">
+
+            <SingleProperty />
+
+        </div>
+    );
+};
+
+export default SwiperExtensionProperty;
